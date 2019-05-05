070b0dc2b9d5cbdab243bcce43ed143ea28e704c
Itay <Itay242@gmail.com>
2019-05-05 10:40:41 +0300

    Adding client

--- game-server/server.py
+++ game-server/server.py
@@ -1,18 +1,25 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import socket
 
 HOST = '127.0.0.1'
-PORT = 65432
+UNITY_PORT = 65432
+OUTSIDE_PORT = 8000
+game_instances = {}
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
+s.bind((HOST, UNITY_PORT))
 s.listen()
 conn, addr = s.accept()
 
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+# @app.route('/create_game_instance'):
+# def
+
 
 
 @app.route('/ook')
@@ -29,6 +36,6 @@ def push():
 
 	return jsonify('ok')
 
-app.run('localhost', 8000)
 
+app.run(HOST, OUTSIDE_PORT, ssl_context='adhoc')
 s.close()
